93951b40f78a511ba3e4db42d24299d7b1af7474
R4NU54 <102753249+R4NU54@users.noreply.github.com>
2022-06-06 19:26:31 +0200

    Add files via upload
    Se añade Ejercicios tema 3 Primera parte.

---
+++ Curso de Introducción a la programación/Ejercicos_tema_3_Parte1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author R4NU54
+ */
+public class Ejercicos_tema_3_Parte1 {
+
+    public static void main(String[] args) {
+        int resultado = 0;
+        resultado = sumado(15, 22, 48);
+        System.out.println(resultado);
+    }
+
+    public static int sumado(int a, int b, int c) {
+        return a + b + c;
+
+    }
+}
